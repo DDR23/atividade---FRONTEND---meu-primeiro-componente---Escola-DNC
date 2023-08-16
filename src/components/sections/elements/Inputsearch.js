0ad831c1d94ecb377daaca3ae314c3a9cd176c13
André Campos <roboreserva.andre@gmail.com>
2023-08-16 20:44:25 -0300

    adicionando ajustes finais do componente Inputsearch

--- src/components/sections/elements/Inputsearch.js
+++ src/components/sections/elements/Inputsearch.js
@@ -6,10 +6,10 @@ import { LiaKeyboardSolid } from "react-icons/lia";
 function Inputsearch(){
     return(
         <div className={styles.inputsearch}>
+            <label className={styles.iconsearch} for="caixa"><IoIosSearch size={20}/></label>
+            <input id="caixa" className={styles.inputsearchinput} type="text" placeholder="Quantos mundiais o Palmeiras tem?"></input>
             <div className={styles.iconsearch}>
-                <IoIosSearch size={20}/>
             </div>
-            <input className={styles.inputsearchinput} type="text" placeholder="Quantos mundiais o Palmeiras tem?"></input>
             <div className={styles.iconsearch}>
                 <a href='#'><LiaKeyboardSolid size={20}/></a>
             </div>
